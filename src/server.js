d729b24639def64f0f0da0753cdd8f812d868c0a
lulucasalves <lucas.alves.supus@outlook.com>
2022-03-28 17:46:15 -0300

    renovate server port

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const app = require('./api/v1.0.0/infra/app')
 require('dotenv/config')
 
 const port = process.env.PORT || 5000
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}\nhttp://localhost:${port}`)
-  })
-}
+// if (process.env.NODE_ENV !== 'test') {
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}\nhttp://localhost:${port}`)
+})
+// }
