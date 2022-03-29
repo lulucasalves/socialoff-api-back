7abbfa979a999960beec6a609740980c4a4cdb66
lulucasalves <lucas.alves.supus@outlook.com>
2022-03-29 00:26:00 -0300

    vercel test new

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('./src/api/v1.0.0/infra/app')
+require('dotenv/config')
+
+const port = process.env.PORT || 5000
+// if (process.env.NODE_ENV !== 'test') {
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}\nhttp://localhost:${port}`)
+})
+// }
